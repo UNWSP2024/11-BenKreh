716e864b413340f8fe5a3ab8c55ff2dcc75b46d2
BenKreh <BenKrehbiel888@gmail.com>
2025-04-11 14:19:00 -0500

    Add files via upload

---
+++ Tkinter first steps.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import *
+
+
+
+saying = Tk()
+saying.geometry("500x500")
+saying.title("My Favourite saying")
+
+
+textbox = tkinter.Text(saying, height=1, width=34)
+textbox.pack(pady=10
+             )
+
+textbox.insert(tkinter.END, "somewhere in the heavens, we exist")
+saying.mainloop()
